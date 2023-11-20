c3d28740c92343bbf3f4dc090807c1a9a20e94f4
Felix Kipping <f.kipping@mds.eu>
2023-11-20 13:36:51 +0100

    refactor: ProjectsManager::$project to non static member variable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Features
 
+- Update to `mds-agenturgruppe/pimprint-core-bundle:3.2.0`
+
+## [3.1.0] - 2023-10-11
+
+### Features
+
 - Documentation of `CoreBundle` v3.1.0 SVG support.
 
 ## [3.0.0] - 2023-07-28
--- src/Project/CommandDemo/ImageBox.php
+++ src/Project/CommandDemo/ImageBox.php
@@ -16,7 +16,6 @@ namespace Mds\PimPrint\DemoBundle\Project\CommandDemo;
 use League\Flysystem\FilesystemException;
 use Mds\PimPrint\CoreBundle\InDesign\Command\GoToPage;
 use Mds\PimPrint\CoreBundle\InDesign\Command\ImageBox as ImageBoxCommand;
-use Mds\PimPrint\CoreBundle\Service\ProjectsManager;
 use Pimcore\Model\Asset;
 
 /**
@@ -169,8 +168,7 @@ class ImageBox extends AbstractStrategy
 
         $asset = $this->loadRandomAsset('/Brand Logos/%', null, ['image/svg+xml']);
         if (false === $asset instanceof Asset) {
-            ProjectsManager::getProject()
-                           ->addPageMessage('No SVG Demo-Asset found.');
+            $this->project->addPageMessage('No SVG Demo-Asset found.');
         } else {
             //Scalable Vector Graphics (SVG) support was dropped with CS4, but resumed with version CC 2020 (15.0)
             //SVG support can be disabled via configuration `mds_pim_print_core.svg_support`
@@ -183,21 +181,18 @@ class ImageBox extends AbstractStrategy
             } catch (\Exception $e) {
                 //`mds_pim_print_core.svg_support`: false
                 //setAsset throws an exception when asset isn't usable in InDesign
-                ProjectsManager::getProject()
-                               ->addPageMessage($e->getMessage());
-
-                //When setting an asset the name of a thumbnail config can be used to use thumbnails and not the
-                //original asset.
-                $imageBox->setAsset($asset, 'product_detail');
+                $this->project->addPageMessage($e->getMessage());
             }
+            //When setting an asset the name of a thumbnail config can be used to use thumbnails an not the
+            //original asset.
+            $imageBox->setAsset($asset, 'product_detail');
             $this->addCommand($imageBox);
             $topPosition += $height + $margin;
         }
 
         $asset = $this->loadRandomAsset('/Sample Content/Documents/%', null, ['application/pdf']);
         if (false === $asset instanceof Asset) {
-            ProjectsManager::getProject()
-                           ->addPageMessage('No PDF Demo-Asset found.');
+            $this->project->addPageMessage('No PDF Demo-Asset found.');
         } else {
             //PDFs can be placed natively in InDesign
             $imageBox = new ImageBoxCommand('image', $left, $topPosition);
